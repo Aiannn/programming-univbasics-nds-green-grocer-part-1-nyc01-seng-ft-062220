83cc87fe30b69bcf575e220631114b6bcf4682d0
Aian Soktoev <soktoevayan@gmail.com>
2020-06-14 17:01:04 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -25,7 +25,7 @@ def consolidate_cart(cart)
   new_cart = []
   
   cart.each do |grocery_item|
-  grocery_item = find_item_by_name_in_collection
+  current_item = find_item_by_name_in_collection(grocery_item[:item], cart)
 end
 
 consolidate_cart(a) 
