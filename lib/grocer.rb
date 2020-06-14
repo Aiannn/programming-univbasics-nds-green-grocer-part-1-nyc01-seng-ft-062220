11b84267a545c90f09ff34892c2df70d27450756
Aian Soktoev <soktoevayan@gmail.com>
2020-06-14 17:07:16 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -21,12 +21,26 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  newArr=[]
-  cart.each do |element|
-    element[:count] = 8
-    newArr.push(element)
+  index = 0
+  new_cart = []
+  
+  cart.each do |grocery_item|
+  current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+  if current_item
+    new_cart_index = 0 
+    new_cart.each do |new_cart_item|
+      if new_cart_item[:item] == current_item[:item]
+        new_cart_item[:count]+=1
+      end
+      new_cart_index+=1 
+    end
+  else   
+    grocery_item[:count] = 1
+    new_cart.push(grocery_item)
   end
-  puts newArr
+  index+=1 
+end
+new_cart
 end
 
 consolidate_cart(a) 
